ccc646fad766433acf80d1ccfe3eaf4193b1e771
xandro2021 <xandrocastro2021@gmail.com>
2023-07-06 17:09:09 -0600

    creacion de componente buttom, info eventos personalizados

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,76 @@
 <script setup>
+  import { ref, computed } from 'vue';
+  import Header from './components/Header.vue';
+  import Button from './components/Button.vue';
+
+  const cantidad = ref(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange(e) {
+    cantidad.value = +e.target.value;
+  }
+
+  const formatearDinero = computed( () => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+
+    return formatter.format(cantidad.value);
+  });
+
+  const handleChangeDecremento = () => {
+    const valor = cantidad.value - STEP;
+
+    if(valor < MIN) {
+      return;
+    }
+    cantidad.value = valor;
+  }
+
+  const handleChangeIncremento = () => {
+    const valor = cantidad.value + STEP;
+
+    if(valor > MAX) {
+      return;
+    }
+    cantidad.value = valor;
+  }
+
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header/>
 
+    <div class="flex justify-between mt-10">
+      <Button
+        :operador="'-'"
+        @fn="handleChangeDecremento"
+      />
+      <Button
+        :operador="'+'"
+        @fn="handleChangeIncremento"
+      />
+    </div>
+
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        :value="cantidad"
+        @input="handleChange"
+      />
+
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {{formatearDinero}}
+      </p>
+    </div>
   </div>
 </template>
