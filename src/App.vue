e56ae5aa948e484b30f2cb3e9b27e06dde344483
xandro2021 <xandrocastro2021@gmail.com>
2023-07-06 18:06:02 -0600

    finalizacion de miniproyecto introductorio a Vue (curso JS Juan)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-  import { ref, computed } from 'vue';
+  import { ref, computed, watch } from 'vue';
   import Header from './components/Header.vue';
   import Button from './components/Button.vue';
+  import { calcularTotalPagar } from './helpers';
 
   const cantidad = ref(10000);
-
+  const meses = ref(6);
+// const total = ref(calcularTotalPagar(cantidad.value, meses.value));
+  const total = ref(0);
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -13,13 +16,25 @@
     cantidad.value = +e.target.value;
   }
 
-  const formatearDinero = computed( () => {
+  const formatearDinero = (valor) => {
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
     });
 
-    return formatter.format(cantidad.value);
+    return formatter.format(valor);
+  };
+
+// watch([cantidad, meses], () => {
+//   total.value = calcularTotalPagar(cantidad.value, meses.value);
+// }, { immediate: true })
+
+  watch([cantidad, meses], () => {
+    total.value = calcularTotalPagar(cantidad.value, meses.value);
+  });
+
+  const pagoMensual = computed(() => {
+    return total.value / meses.value;
   });
 
   const handleChangeDecremento = () => {
@@ -69,8 +84,38 @@
       />
 
       <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">
-        {{formatearDinero}}
+        {{formatearDinero(cantidad)}}
       </p>
+
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span class="text-indigo-600">Plazo</span> a pagar
+      </h2>
+
+      <select
+        class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-5"
+        :value="meses"
+        v-model.number="meses"
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+
     </div>
+
+    <div v-if="total > 0" class="my-5 space-y-3 bg-gray-50 p-5">
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Resumen <span class="text-indigo-600">de pagos</span>
+      </h2>
+
+      <p class="text-xl text-gray-500 text-center font-bold">{{meses}} Meses</p>
+      <p class="text-xl text-gray-500 text-center font-bold">Total a Pagar: {{formatearDinero(total)}}</p>
+      <p class="text-xl text-gray-500 text-center font-bold">Mensuales: {{ formatearDinero(pagoMensual) }}</p>
+    </div>
+
+    <div v-else class="text-center">
+      Agrega una cantidad y un plazo a pagar
+    </div>
+
   </div>
 </template>
